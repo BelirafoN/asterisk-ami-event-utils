292dbf2fce375b585181e8e01e01bf7f73748d91
Alexandr Voronyansky <belirafon@gmail.com>
2016-04-15 13:32:57 +0300

    added all files of library

---
+++ README.md
@@ -0,0 +1,53 @@
+# Asterisk AMI Event Utils for NodeJS (ES2015)
+
+Some service functions for parse\stringify raw events from Asterisk AMI's socket. 
+This library is a part of other library for work with Asterisk AMI, which will be release soon.
+
+### Available methods 
+
+* `.toString(eventBuffer)` - converts raw AMI's event from buffer to utf-8 string
+and trim all CRLF in start or end of this string;
+* `.toObject(eventBuffer)` - converts raw AMI's event from buffer to object;
+* `.toJson(eventBuffer)` - converts raw AMI's event from buffer to json-string;
+* `.toArray(eventBuffer)` - converts raw AMI's event from buffer to paired-array, 
+like this: *[['prop_name_1', 'prop_val_1'], ..., ['prop_name_n', 'prop_val_n']]*;
+* `.fromObject(eventObject)` - converts event from object to string, which ready for send into  
+socket of Asterisk AMI socket; 
+* `.fromArray(eventArray)` - converts event from paired-array to string, which ready for send into  
+socket of Asterisk AMI socket;
+
+### Usage 
+
+```javascript
+const eventUtils = require('asterisk-ami-event-utils');
+let eventBuffer = '...'; // any buffer or string of event from AMI's socket or from other place
+let eventObject = eventUtils.toObject(eventBuffer);
+```
+
+### Examples 
+
+For examples, please, see tests `./test/*`.
+
+### Tests 
+
+Tests require [Mocha](https://mochajs.org/). 
+
+```bash 
+mocha ./tests
+``` 
+
+or with `npm` 
+
+```bash
+npm test 
+```
+
+Test coverage with [Istanbul](https://gotwarlost.github.io/istanbul/) 
+
+```bash
+npm run coverage
+```
+
+### License 
+
+Licensed under the MIT License
---
+++ test/utilsTest.js
@@ -0,0 +1,159 @@
+/**
+ * Developer: BelirafoN
+ * Date: 14.04.2016
+ * Time: 18:31
+ */
+
+"use strict";
+
+const assert = require('assert');
+const eventUtil = require('../lib');
+const CRLF = '\r\n';
+
+describe('Event utils test', () => {
+    let rawEvent = [
+        'Event: AgentRingNoAnswer',
+        'Privilege: agent,all',
+        'Queue: 592'
+    ].join(CRLF),
+        testEvent = null;
+    
+    beforeEach(() => {
+        testEvent = rawEvent.substr(0);
+    });
+
+    it('Check utils content', () => {
+        assert.deepEqual(Object.keys(eventUtil), [
+            'toObject',
+            'toJson',
+            'toArray',
+            'toString',
+            'fromObject',
+            'fromArray'
+        ]);
+    });
+
+    describe('rawToString', () => {
+
+        it('with event\'s raw string', () => {
+            testEvent = CRLF.repeat(5) + testEvent + CRLF.repeat(5);
+            assert.equal(rawEvent, eventUtil.toString(testEvent));
+        });
+
+        it('without event\'s raw string', () => {
+            assert.equal(eventUtil.toString(), '');
+        });
+
+    });
+    
+    describe('rawToObject', () => {
+
+        it('with event\'s raw string', () => {
+            assert.deepEqual(eventUtil.toObject(testEvent), {
+                Event: 'AgentRingNoAnswer',
+                Privilege: 'agent,all',
+                Queue: '592'
+            });
+        });
+
+        it('without event\'s raw string', () => {
+            assert.deepEqual(eventUtil.toObject(), {});
+        });
+
+        it('invalid property pair in event\'s raw string', () => {
+            testEvent = [
+                    'Event: AgentRingNoAnswer',
+                    'Privilege agent,all',
+                    'Queue: 592'
+                ].join(CRLF) + CRLF.repeat(2);
+
+            assert.deepEqual(eventUtil.toObject(testEvent), {
+                Event: 'AgentRingNoAnswer',
+                Queue: '592'
+            });
+        });
+
+    });
+
+    describe('rawToArray', () => {
+
+        it('with event\'s raw string', () => {
+            assert.deepEqual(eventUtil.toArray(testEvent), [
+                ['Event', 'AgentRingNoAnswer'],
+                ['Privilege', 'agent,all'],
+                ['Queue', '592']
+            ]);
+        });
+
+        it('without event\'s raw string', () => {
+            assert.deepEqual(eventUtil.toArray(), []);
+        });
+
+    });
+
+    describe('rawToJson', () => {
+
+        it('with event\'s raw string', () => {
+            assert.equal(eventUtil.toJson(testEvent), '{"Event":"AgentRingNoAnswer","Privilege":"agent,all","Queue":"592"}');
+        });
+
+        it('without event\'s raw string', () => {
+            assert.equal(eventUtil.toJson(), '{}');
+        });
+
+    });
+
+    describe('arrayToRaw', () => {
+
+        it('valid event\'s array', () => {
+            assert.equal(testEvent + CRLF.repeat(2), eventUtil.fromArray([
+                ['Event', 'AgentRingNoAnswer'],
+                ['Privilege', 'agent,all'],
+                ['Queue', '592']
+            ]));
+        });
+
+        it('empty event\'s array', () => {
+            assert.equal('', eventUtil.fromArray([]));
+        });
+
+        it('without event\'s array', () => {
+            assert.equal(''.repeat(2), eventUtil.fromArray());
+        });
+
+        it('invalid element of event\'s array', () => {
+            let expectStr = [
+                    'Event: AgentRingNoAnswer',
+                    'Privilege: agent,all'
+                ].join(CRLF) + CRLF.repeat(2);
+
+            assert.deepEqual(expectStr, eventUtil.fromArray([
+                ['Event', 'AgentRingNoAnswer'],
+                ['Privilege', 'agent,all'],
+                []
+            ]));
+        });
+
+    });
+
+    describe('objectToRaw', () => {
+
+        it('valid event\'s object', () => {
+            assert.equal(testEvent + CRLF.repeat(2), eventUtil.fromObject({
+                Event: 'AgentRingNoAnswer',
+                Privilege: 'agent,all',
+                Queue: '592'
+            }));
+        });
+
+        it('empty event\'s object', () => {
+            assert.equal('', eventUtil.fromObject({}));
+        });
+
+        it('without event\'s object', () => {
+            assert.equal(''.repeat(2), eventUtil.fromObject());
+        });
+
+    });
+    
+});
